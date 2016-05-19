371f7b154950290e5b6a5dac3b456313392dbe69
tss <tss@debian-tss24>
2016-05-19 21:40:38 +0200

    Fine lez

--- src/lezione10/App.java
+++ src/lezione10/App.java
@@ -5,6 +5,10 @@
  */
 package lezione10;
 
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -12,12 +16,24 @@ import javax.swing.JFrame;
  * @author tss
  */
 public class App {
+
     public static void main(String[] args) {
 
-        JFrame f =new JFrame("Esempio swing");
+        JFrame f = new JFrame("Esempio swing");
         f.setBounds(100, 100, 300, 200);
-        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contenitore = f.getContentPane();
+        contenitore.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JButton btn = new JButton("invia");
+        btn.setBounds(10, 10, 100, 50);
+        contenitore.add(btn);
         
+        JButton btn1 = new JButton("invia1");
+        btn1.setPreferredSize(new Dimension(100,200));
+        contenitore.add(btn1);
+        f.setVisible(true);
+
     }
-  
+
 }
